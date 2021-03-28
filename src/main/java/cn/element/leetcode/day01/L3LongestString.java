d7fd1b661593d27f7b6a93c7941a750628bb5a80
Dell <1529146935@qq.com>
2021-03-28 21:21:49 +0800

    做了5题了,简直题题要命

--- src/main/java/cn/element/leetcode/day01/L3LongestString.java
+++ src/main/java/cn/element/leetcode/day01/L3LongestString.java
@@ -1,17 +1,64 @@
 package cn.element.leetcode.day01;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ *
+ * 输入: s = "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ *
+ * 输入: s = "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ *
+ * 输入: s = "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是"wke"，所以其长度为 3。
+ *      请注意，你的答案必须是 子串 的长度，"pwke"是一个子序列，不是子串。
+ *
+ * 输入: s = ""
+ * 输出: 0
  */
 public class L3LongestString {
 
-    public int lengthOfLongestSubstring(String s) {
+    /**
+     * 使用滑动窗口的方法来获得子串
+     */
+    public static int lengthOfLongestSubstring(String s) {
 
+        Set<Character> set = new HashSet<>();//定义一个hashset用于记录字符是否出现过
 
-        return 0;
+        int n = s.length();//定义变量n接收字符串的长度
+
+        int rk = -1,ans = 0;//右指针,初始值为-1,相当于我们在字符串左边界的左侧,还没有开始移动,ans指的是子串的长度
+
+        for (int i = 0; i < n; i++) {//遍历整个字符串
+            if(i != 0){
+                set.remove(s.charAt(i - 1));//左指针向右移动一格,移除一个字符
+            }
+
+            while(rk + 1 < n && !set.contains(s.charAt(rk+1))){
+                set.add(s.charAt(rk+1));//不断地移动右指针
+                ++rk;
+            }
+
+            //第i到rk个字符是一个极长的无重复字符子串
+            ans = Math.max(ans,rk-i+1);
+        }
+
+        return ans;
     }
 
     public static void main(String[] args) {
+
+        String str = "pwwkew";
+        int count = lengthOfLongestSubstring(str);
+
+        System.out.println(count);
+
 
     }
 }
---
+++ src/main/java/cn/element/leetcode/day01/L5LongestPalindromicSubstring.java
@@ -0,0 +1,65 @@
+package cn.element.leetcode.day01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给你一个字符串 s，找到 s 中最长的回文子串。
+ *
+ * 输入：s = "babad"
+ * 输出："bab"
+ * 解释："aba" 同样是符合题意的答案。
+ *
+ * 输入：s = "cbbd"
+ * 输出："bb"
+ *
+ * 输入：s = "a"
+ * 输出："a"
+ *
+ * 输入：s = "ac"
+ * 输出："a"
+ */
+public class L5LongestPalindromicSubstring {
+
+    /**
+     * 使用动态规划来解题
+     */
+    public static String longestPalindrome(String s) {
+
+        int n = s.length();//求出字符串的长度
+
+        boolean[][] dp = new boolean[n][n];//声明一个二维数组用来记录状态
+
+        String str = "";//用于接收最终的字符串
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j + i < n; j++) {
+                int k = i + j;
+
+                if(i == 0){
+                    dp[j][k] = true;
+                }else if(i == 1){
+                    dp[j][k] = s.charAt(j) == s.charAt(k);
+                }else{
+                    dp[j][k] = s.charAt(j) == s.charAt(k) && dp[j+1][k-1];
+                }
+
+                if(dp[j][k] && i + 1 > str.length()){
+                    str = s.substring(j,j + i + 1);
+                }
+                
+            }
+        }
+
+        return str;
+    }
+
+    public static void main(String[] args) {
+
+        String s = longestPalindrome("babad");
+
+        System.out.println(s);
+    }
+
+
+}
