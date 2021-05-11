30bc3a2e0f5cd94277bbf53948daac08a0e0083a
Dell <1529146935@qq.com>
2021-05-11 18:05:36 +0800

    49

---
+++ src/main/java/cn/element/leetcode/stage2/L49GroupAnagrams.java
@@ -0,0 +1,51 @@
+package cn.element.leetcode.stage2;
+
+import java.util.*;
+
+/**
+ * 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+ *
+ * 示例:
+ *
+ * 输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+ * 输出:
+ * [
+ *   ["ate","eat","tea"],
+ *   ["nat","tan"],
+ *   ["bat"]
+ * ]
+ */
+public class L49GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String,List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+
+            Arrays.sort(chars);
+
+            String key = new String(chars);
+
+            List<String> list = map.getOrDefault(key,new ArrayList<>());
+
+            list.add(str);
+
+            map.put(key,list);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+
+        L49GroupAnagrams a = new L49GroupAnagrams();
+
+        String[] strArr = {"eat", "tea", "tan", "ate", "nat", "bat"};
+
+        List<List<String>> lists = a.groupAnagrams(strArr);
+
+        lists.forEach(System.out::println);
+    }
+}
